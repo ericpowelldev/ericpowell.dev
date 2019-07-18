bfb63b37e6287214f0d794f3887a159d61961ee3
epowell7 <ericpowell777@gmail.com>
2019-07-18 15:30:20 -0400

    added work page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,36 @@
 $(window).on('load', function () {
+    showProfile();
     $("#cover").hide();
 });
+
+function hideHTML() {
+    $("#profile").hide();
+    $("#work").hide();
+    $("#contact").hide();
+    scroll(0,0);
+}
+function showProfile() {
+    hideHTML();
+    $(".navBtn#nProfile").addClass("navBtnActive");
+    $(".navBtn#nWork").removeClass("navBtnActive");
+    $(".navBtn#nContact").removeClass("navBtnActive");
+    $("#profile").show();
+}
+function showWork() {
+    hideHTML();
+    $(".navBtn#nProfile").removeClass("navBtnActive");
+    $(".navBtn#nWork").addClass("navBtnActive");
+    $(".navBtn#nContact").removeClass("navBtnActive");
+    $("#work").show();
+}
+function showContact() {
+    hideHTML();
+    $(".navBtn#nProfile").removeClass("navBtnActive");
+    $(".navBtn#nWork").removeClass("navBtnActive");
+    $(".navBtn#nContact").addClass("navBtnActive");
+    $("#contact").show();
+}
+
+$(document).on("click", "#nProfile", showProfile);
+$(document).on("click", "#nWork", showWork);
+$(document).on("click", "#nContact", showContact);
